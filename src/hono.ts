5eed880701adc722b01e6efc225c32ac4fca2da7
ricemountainer <rmrmrmarmrmrm@gmail.com>
2026-04-24 00:43:13 +0900

    fix

---
+++ src/hono.ts
@@ -0,0 +1,25 @@
+import {Hono} from 'hono';
+
+import slackAppHttp from './slack/slackapp-http.ts';
+
+const app = new Hono();
+app.get('/', (c) => c.text('Hello Node.js!'));
+app.post('/slack', async (c)=>{
+    const cloneReq = c.req.raw.clone(); 
+    const body = await cloneReq.json();
+    const challenge = body['challenge'];
+    await slackAppHttp(c.req.raw);
+    return c.text(challenge, 200);
+});
+app.post('/slack/commands', async (c)=>{
+    // for Slash Command
+    const r = await slackAppHttp(c.req.raw);
+    return r;
+});
+app.post('/slack/interactive' , async (c)=>{
+    // for Interactive Component & Shortcuts
+    const r = await slackAppHttp(c.req.raw);
+    return r;
+});
+export default app;
+
--- src/start-hono-server.ts
+++ src/start-hono-server.ts
@@ -1,31 +1,9 @@
 import { serve } from '@hono/node-server';
-import {Hono} from 'hono';
-
+import app from './hono.ts';
 import dotenv from 'dotenv';
 dotenv.config();
-import slackAppHttp from './slack/slackapp-http.ts';
-
 const port = Number(process.env.PORT!||3000);
 
-const app = new Hono();
-app.get('/', (c) => c.text('Hello Node.js!'));
-app.post('/slack', async (c)=>{
-    const cloneReq = c.req.raw.clone(); 
-    const body = await cloneReq.json();
-    const challenge = body['challenge'];
-    await slackAppHttp(c.req.raw);
-    return c.text(challenge, 200);
-});
-app.post('/slack/commands', async (c)=>{
-    console.log('debug: /slack/commands was requested');
-    const r = await slackAppHttp(c.req.raw);
-    return r;
-});
-app.post('/slack/interactive' , async (c)=>{
-    console.log('debug: /slack/interactive was requested');
-    const r = await slackAppHttp(c.req.raw);
-    return r;
-});
 serve({
     fetch: app.fetch,
     port: port,
